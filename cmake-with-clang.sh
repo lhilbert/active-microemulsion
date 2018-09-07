45009b9c828263f7a68f54ecda222aeb8ad6d37b
Tommaso Bianucci <churli@users.noreply.github.com>
2018-09-07 16:37:34 +0200

    Fixing the cmake-based build scripts.

--- cmake-with-clang.sh
+++ cmake-with-clang.sh
@@ -8,5 +8,5 @@ BUILD_DIR="${SRC_DIR}/cmake-build-${CC}"
 mkdir -p ${BUILD_DIR}
 pushd ${BUILD_DIR}
 cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ${SRC_DIR}
-cmake --build ${BUILD_DIR} --target active_microemulsion -- -j 2
+cmake --build ${BUILD_DIR} --target active-microemulsion -- -j 2
 popd
