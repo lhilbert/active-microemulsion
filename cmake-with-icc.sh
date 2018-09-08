b27796b1de06f5cfaf5a89523af19597483d4499
Tommaso Bianucci <churli@users.noreply.github.com>
2018-09-08 10:23:23 +0200

    Auto-linking the binary generated with cmake scripts into the base directory.

--- cmake-with-icc.sh
+++ cmake-with-icc.sh
@@ -2,11 +2,16 @@
 export CC=icc
 export CXX=icpc
 
+BUILD_TYPE="Debug"
+#BUILD_TYPE="Release"
+
 SRC_DIR="$(pwd)"
 BUILD_DIR="${SRC_DIR}/cmake-build-${CC}"
+TARGET="active-microemulsion"
 
 mkdir -p ${BUILD_DIR}
 pushd ${BUILD_DIR}
-cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ${SRC_DIR}
-cmake --build ${BUILD_DIR} --target active-microemulsion -- -j 2
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G "CodeBlocks - Unix Makefiles" ${SRC_DIR}
+cmake --build ${BUILD_DIR} --target ${TARGET} -- -j 2
 popd
+ln -sf ${BUILD_DIR}/${TARGET} ${SRC_DIR}/.
