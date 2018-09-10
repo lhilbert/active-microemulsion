10a64bb38bb7a935eed5523dbe03067667cffd69
Tommaso Bianucci <churli@users.noreply.github.com>
2018-09-10 10:36:54 +0200

    Fixed slurm-job.sh example job so that it actually works.

--- slurm-job.sh
+++ slurm-job.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Active microemulsion command line parameters
-CONFIG_FLAGS="-T 12e0 -c 3 -w 0.25 -s 333 --no-sticky-boundary -P ChainConfigs/testConfig.chains"
+CONFIG_FLAGS="-T 18e3 -c 3 -w 0.25 -s 333 --no-sticky-boundary -P ChainConfigs/testConfig.chains"
 
 # OutFolder naming filter from flags
 flagsToNameFilter()
 {
     sed s/" -\| --"/"_"/g | sed s/"[.]\|^-"/""/g | sed s,"_[a-zA-Z] [^ ]\+/[^ ]\+","",g | sed s/" "/""/g
 }
-OUT_DIR="Out_$(echo ${CONFIG_FLAGS} | flagsToNameFilter)_${SLURM_JOB_ID}"
+OUT_DIR="Out_$(echo ${CONFIG_FLAGS} | flagsToNameFilter)_slurm_${SLURM_JOB_ID}"
 
 # Settings for config data and shared libraries
 REPO_BASE_DIR=${HOME}/Repo/active-microemulsion
-REPO_ITEMS_TO_COPY=( "active-microemulsion" "lib" "ChainConfigs" "sequenceFigureBuilder.sh" ) # Will be copied with "cp -r"
+REPO_ITEMS_TO_COPY=( "active-microemulsion" "cmake-build-*" "lib" "ChainConfigs" "sequenceFigureBuilder.sh" ) # Will be copied with "cp -r"
 
 ################
 #
@@ -23,33 +23,33 @@ REPO_ITEMS_TO_COPY=( "active-microemulsion" "lib" "ChainConfigs" "sequenceFigure
 # lines starting with "#SBATCH" define your jobs parameters
 
 # requesting the type of node on which to run job
-#SBATCH --partition <patition name>
+##SBATCH --partition <patition name>
 
 # telling slurm how many instances of this job to spawn (typically 1)
-SBATCH --ntasks 1
+#SBATCH --ntasks 1
 
 # setting number of CPUs per task (1 for serial jobs)
-SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 1
 
 # setting memory requirements (in MB)
-SBATCH --mem-per-cpu 512
+#SBATCH --mem-per-cpu 512
 
 # propagating max time for job to run
 ##SBATCH --time <days-hours:minute:seconds>
 ##SBATCH --time <hours:minute:seconds>
 ##SBATCH --time <minutes>
-SBATCH --time 60
+#SBATCH --time 60
 
 # Setting the name for the job
-SBATCH --job-name active-microemulsion
+#SBATCH --job-name active-microemulsion
 
 # setting notifications for job
 # accepted values are ALL, BEGIN, END, FAIL, REQUEUE
-SBATCH --mail-type ALL
+#SBATCH --mail-type ALL
 
 # telling slurm where to write output and error
-#SBATCH --output <path>-%j.out
-#SBATCH --error <path>-%j.out
+##SBATCH --output <path>-%j.out
+##SBATCH --error <path>-%j.out
 
 ################
 #
@@ -63,7 +63,9 @@ mkdir -p ${scratch}
 
 # copy all your NEEDED data to ComputeNode
 for item in ${REPO_ITEMS_TO_COPY[@]}; do
-    cp -r ${REPO_BASE_DIR}/${item} ${scratch}/.
+    CMD=cp -r ${REPO_BASE_DIR}/${item} ${scratch}/.
+    echo ${CMD}
+    ${CMD}
 done
 
 # Entering scratch directory
