0a9b9171823ca6979fe28bdf7746ebf17688e4b6
Tommaso Bianucci <churli@users.noreply.github.com>
2018-11-30 19:42:29 +0100

    Updated cmake-with-icc script.

--- cmake-with-icc.sh
+++ cmake-with-icc.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+
+if command -v module 1>/dev/null 2>&1; then
+    module load lib/boost devel/cmake/3.11.1
+    module unload compiler/gnu
+    module load compiler/intel
+fi
+
 export CC=icc
 export CXX=icpc
 
-BUILD_TYPE="Debug"
-#BUILD_TYPE="Release"
+#BUILD_TYPE="Debug"
+#BUILD_TYPE="PGO"
+BUILD_TYPE="Release"
 
 SRC_DIR="$(pwd)"
 BUILD_DIR="${SRC_DIR}/cmake-build-${CC}"
@@ -14,4 +22,4 @@ pushd ${BUILD_DIR}
 cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G "CodeBlocks - Unix Makefiles" ${SRC_DIR}
 cmake --build ${BUILD_DIR} --target ${TARGET} -- -j 2
 popd
-ln -sf ${BUILD_DIR}/${TARGET} ${SRC_DIR}/.
+ln -sf ${BUILD_DIR}/src/${TARGET} ${SRC_DIR}/.
