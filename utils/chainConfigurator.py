fd47f5446d75ab3aa85538ad3b35d0da6bad16d8
Tommaso Bianucci <churli@users.noreply.github.com>
2019-01-24 17:18:15 +0100

    Add sparse initialization feature to chainConfigurator script
    Now it is possible to have empty space interposed between chromatin at initialization, this feature is available with the "-s" or "--sparse" flags.

--- utils/chainConfigurator.py
+++ utils/chainConfigurator.py
@@ -42,7 +42,7 @@ class Chromosome:
     species = "CHROMATIN"
 
     def __init__(self, boxBaseCoordinates, boxDimensions, occupancy, rotation, startCorner, active=0, transcribable=0,
-                 inhibited=0, cutoff=1):
+                 inhibited=0, cutoff=1, sparseMode=False):
         self.active = active
         self.transcribable = transcribable
         self.inhibited = inhibited
@@ -52,6 +52,7 @@ class Chromosome:
         self.startPositionOffset = [0, 0]  # The offset of the start corner relative to the box base coordinates
         self.startPosition = self.boxBaseCoordinates
         self.occupancy = occupancy
+        self.sparseMode = sparseMode # Sparse mode is when we leave some space within a chromatin region
         self.rotation = rotation
         self.startCorner = startCorner
         self.initialDirections = ['+', '+']
@@ -88,46 +89,65 @@ class Chromosome:
         self.length = round(boxArea * self.occupancy)
 
     def __buildSteps(self):
+        numRows = self.boxDimensions[1]
+        numCols = self.boxDimensions[0]
         if self.rotation == 'h':
-            numRows = self.boxDimensions[1]
-            numCols = self.boxDimensions[0]
-            counter = 1
-            rowCounter = 1
-            horizontalDirectionNum = direction2Num[self.initialDirections[0]]
-            verticalDirection = self.initialDirections[1]
-            while counter <= self.length and rowCounter <= numRows:
-                stepsToGo = self.length - counter
-                if stepsToGo < (numCols - 1):
-                    self.steps.append(Step.horizontalStep(stepsToGo, num2Direction[horizontalDirectionNum]))
-                    counter += stepsToGo
-                else:
-                    self.steps.append(Step.horizontalStep(numCols - 1, num2Direction[horizontalDirectionNum]))
-                    counter += numCols - 1
-                self.steps.append(Step.verticalStep(stepsize=verticalDirection))
-                counter += 1
-                rowCounter += 1
-                horizontalDirectionNum = (horizontalDirectionNum + 1) & 1  # Here we %2 so we always have 0 or 1
-            self.steps.pop()  # Removing the last vertical step to stay within bounds
+            directionLimit = numRows
+            otherDirectionLimit = numCols
+            direction = self.initialDirections[0]
+            otherDirection = self.initialDirections[1]
+            directionStep = Step.horizontalStep
+            otherDirectionStep = Step.verticalStep
+            self.__buildStepsInDirection(direction, otherDirection, directionStep, otherDirectionStep, directionLimit,
+                                         otherDirectionLimit)
         elif self.rotation == 'v':
-            numRows = self.boxDimensions[1]
-            numCols = self.boxDimensions[0]
-            counter = 1
-            colCounter = 1
-            verticalDirectionNum = direction2Num[self.initialDirections[1]]
-            horizontalDirection = self.initialDirections[0]
-            while counter <= self.length and colCounter <= numCols:
-                stepsToGo = self.length - counter
-                if stepsToGo < (numRows - 1):
-                    self.steps.append(Step.verticalStep(stepsToGo, num2Direction[verticalDirectionNum]))
-                    counter += stepsToGo
-                else:
-                    self.steps.append(Step.verticalStep(numRows - 1, num2Direction[verticalDirectionNum]))
-                    counter += numRows - 1
-                self.steps.append(Step.horizontalStep(stepsize=horizontalDirection))
-                counter += 1
-                colCounter += 1
-                verticalDirectionNum = (verticalDirectionNum + 1) & 1  # Here we %2 so we always have 0 or 1
-        self.steps.pop()  # Removing the last vertical step to stay within bounds
+            directionLimit = numCols
+            otherDirectionLimit = numRows
+            direction = self.initialDirections[1]
+            otherDirection = self.initialDirections[0]
+            directionStep = Step.verticalStep
+            otherDirectionStep = Step.horizontalStep
+            self.__buildStepsInDirection(direction, otherDirection, directionStep, otherDirectionStep, directionLimit,
+                                         otherDirectionLimit)
+
+    def __buildStepsInDirection(self, direction, otherDirection, directionStep, otherDirectionStep, directionLimit,
+                                otherDirectionLimit):
+        counter = 1
+        lineCounter = 1
+        directionNum = direction2Num[direction]
+        # Define the stride in the direction (depends on sparseMode)
+        strideTheoretical = 1
+        if (self.sparseMode):
+            strideTheoretical = 1.0/self.occupancy # Be careful, float stride now
+        strideAccumulator = 0.0
+
+        while counter < self.length and lineCounter < directionLimit:
+            stepsToGo = self.length - counter
+            # Steps in the otherDirection (long ones)
+            if stepsToGo < (otherDirectionLimit - 1):
+                self.steps.append(directionStep(stepsToGo, num2Direction[directionNum]))
+                counter += stepsToGo
+            else:
+                self.steps.append(directionStep(otherDirectionLimit - 1, num2Direction[directionNum]))
+                counter += otherDirectionLimit - 1
+            # Steps in the direction (short ones)
+            #  Here compute the actual stride and keep track of any remaining part
+            stride = int(floor(strideTheoretical))
+            strideAccumulator += strideTheoretical - stride
+            if (floor(strideAccumulator) > 0):
+                stride += int(floor(strideAccumulator))
+                strideAccumulator -= floor(strideAccumulator)
+            #  Now make sure not to make too many steps
+            stepsToGo = self.length - counter
+            if stepsToGo <= 0: # Make sure not to write a 0 step
+                break
+            stride = min(stride, stepsToGo)
+            #  Now actually perform the steps in direction
+            self.steps.append(otherDirectionStep(multiplier=stride, stepsize=otherDirection))
+            counter += stride
+            lineCounter += stride
+            directionNum = (directionNum + 1) & 1  # Here we %2 so we always have 0 or 1
+        # self.steps.pop()  # Removing the last orthogonal step to stay within bounds
 
     def __repr__(self):
         return "%s,Active=%d,Transcribable=%d,Inhibited=%d,Cutoff=%d" % (self.species,
@@ -141,12 +161,14 @@ class Chromosome:
 
 class Configurator:
 
-    def __init__(self, domainDimensions, numChromosomes, occupancy, inhibitionProbability, numActiveChains=-1):
+    def __init__(self, domainDimensions, numChromosomes, occupancy, inhibitionProbability, numActiveChains=-1,
+                 sparseMode=False):
         self.domainDimensions = domainDimensions
         self.numChromosomes = numChromosomes
         self.occupancy = occupancy
         self.inhibitionProb = inhibitionProbability
         self.numActiveChains = numActiveChains
+        self.sparseMode = sparseMode
         self.chromosomeStartPositions = []
         self.chromosomeInhibitionFlags = []
         self.__computeInhibitionFlags()
@@ -154,6 +176,8 @@ class Configurator:
         self.__generateChromosomes()
 
     def __computeInhibitionFlags(self):
+        # TODO: here give the possibility to set just 1 active chain in the exact middle (it will be in the center of
+        #  the domain). It should be driven by a specific cmdline flag.
         if self.numActiveChains < 0:
             # In this case just draw them randomly with probability
             self.chromosomeInhibitionFlags = \
@@ -180,7 +204,8 @@ class Configurator:
                         0,
                         0,
                         inhibitFlag,
-                        int(not inhibitFlag))
+                        int(not inhibitFlag),
+                        sparseMode=self.sparseMode)
              for coord, inhibitFlag in zip(self.chromosomeStartPositions, self.chromosomeInhibitionFlags)]
 
     def __repr__(self):
@@ -216,6 +241,8 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--number-of-chains", help="The number of chromatin chains to be generated",
                         dest="numChains", type=int,
                         default=25)
+    parser.add_argument("-s", "--sparse", help="Initialize chromatin blocks in the sparsest way possible (more spacing)",
+                        dest="sparse", action='store_true')
 
     args = parser.parse_args()
     #
@@ -229,7 +256,7 @@ if __name__ == "__main__":
         args.numActiveChains = args.numChains - 1
 
     chainConfigurator = Configurator((args.width, args.height), args.numChains, args.chromatinRatio,
-                                     args.inhibitionProbability, args.numActiveChains)
+                                     args.inhibitionProbability, args.numActiveChains, sparseMode=args.sparse)
     with open(args.outputFile, 'w') as f:
         f.write(str(chainConfigurator))
 
